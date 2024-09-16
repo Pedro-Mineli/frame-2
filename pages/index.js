300e06e799ac1c35aa8ded64905fc790a73bc49e
Pedro-Mineli <mineli.pedro@gmail.com>
2024-09-16 15:03:17 +0000

    aia

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Titulo from"./titulo";
 function index(){
     return <div>
         <Titulo titulo= " Bem-Vindo Turma 6A ao React "/>
-        <CapsLock name= "Pedrin" idade="17"/>
-        <CapsLock name= "Pedrin" idade="17"/>
-        <CapsLock name= "Pedrin" />
-        <CapsLock name= "aiai" idade="17"/>
+        <CapsLock name= "Pedrin" idade="17" cor="red"/>
+        <CapsLock name= "Pedrin" idade="17"cor="blue"/>
+        <CapsLock name= "Pedrin"  cor="pink "/>
+        <CapsLock name= "aiai" idade="17" cor=" green"/>
     </div>
 }
-function CapsLock({name, idade}){
+function CapsLock({name, idade,cor}){
     //console.log(param);
-    return <div>{name} {idade? idade: "n tem idade"}</div>
+    return <div style={{color:cor}}>{name} {idade? idade: "n tem idade"}</div>
 } 
 export default index;
